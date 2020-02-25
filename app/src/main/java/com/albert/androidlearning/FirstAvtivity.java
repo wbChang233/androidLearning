553879146ba07b420e13ef523a42642db446055b
wbChang <2692685036@qq.com>
2020-02-25 16:33:47 +0800

    2.3.2 显式intent

--- app/src/main/java/com/albert/androidlearning/FirstAvtivity.java
+++ app/src/main/java/com/albert/androidlearning/FirstAvtivity.java
@@ -1,5 +1,6 @@
 package com.albert.androidlearning;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -18,7 +19,8 @@ public class FirstAvtivity extends AppCompatActivity {
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                finish();
+                Intent intent = new Intent(FirstAvtivity.this,SecondActivity.class);
+                startActivity(intent);
             }
         });
     }
