a86117862981b5d6b4287b1ab61b8e48e35554c1
wbChang <2692685036@qq.com>
2020-02-25 19:50:16 +0800

    2.5.2 启动模式singtop

--- app/src/main/java/com/albert/androidlearning/FirstAvtivity.java
+++ app/src/main/java/com/albert/androidlearning/FirstAvtivity.java
@@ -19,7 +19,7 @@ public class FirstAvtivity extends AppCompatActivity {
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(FirstAvtivity.this,FirstAvtivity.class);
+                Intent intent = new Intent(FirstAvtivity.this,SecondActivity.class);
                 startActivity(intent);
             }
         });
--- app/src/main/java/com/albert/androidlearning/SecondActivity.java
+++ app/src/main/java/com/albert/androidlearning/SecondActivity.java
@@ -1,7 +1,12 @@
 package com.albert.androidlearning;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import java.lang.reflect.Field;
 
 public class SecondActivity extends AppCompatActivity {
 
@@ -9,5 +14,13 @@ public class SecondActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+        Button button2 = findViewById(R.id.button_2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SecondActivity.this, FirstAvtivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
