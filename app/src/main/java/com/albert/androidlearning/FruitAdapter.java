f6370161c8294fa24989de05f24278c811268b78
wbChang <2692685036@qq.com>
2020-02-26 16:12:13 +0800

    3.5.2 定制ListView的界面

---
+++ app/src/main/java/com/albert/androidlearning/FruitAdapter.java
@@ -0,0 +1,32 @@
+package com.albert.androidlearning;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class FruitAdapter extends ArrayAdapter<Fruit> {
+
+    private int resourceId;
+
+    public FruitAdapter(Context context, int textViewResourceId, List<Fruit> objects){
+        super(context,textViewResourceId,objects);
+        resourceId = textViewResourceId;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        Fruit fruit = getItem(position);
+        View view = LayoutInflater.from(getContext()).inflate(resourceId,parent,false);
+        ImageView fruitImage = view.findViewById(R.id.fruit_image);
+        TextView fruitName = view.findViewById(R.id.fruit_name);
+        fruitImage.setImageResource(fruit.getImageId());
+        fruitName.setText(fruit.getName());
+        return view;
+    }
+}
--- app/src/main/java/com/albert/androidlearning/MainActivity.java
+++ app/src/main/java/com/albert/androidlearning/MainActivity.java
@@ -6,20 +6,50 @@ import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity
 {
 
-    private String[] data = { "1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19"};
+/*    private String[] data = { "Apple","Banana","Watermelon","Pear","Grape","Pineapple","Strawberry",
+            "Cherry","Mango","Apple","Banana","Orange","Watermelon","Pear","Grape","Pineapple","Strawberry",
+            "Cherry","Mango"};*/
 
+    private List<Fruit> fruitList = new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(MainActivity.this,android.R.layout.simple_list_item_1,
-                data);
+        initFruits();
+        FruitAdapter adapter = new FruitAdapter(MainActivity.this,R.layout.fruit_item,fruitList);
         ListView listView = findViewById(R.id.list_view);
         listView.setAdapter(adapter);
 
+    }
+
+    private void initFruits(){
+        for (int i = 0;i < 2;i++){
+            Fruit apple = new Fruit("Apple",R.drawable.img_1);
+            fruitList.add(apple);
+            Fruit apple2 = new Fruit("Apple",R.drawable.img_1);
+            fruitList.add(apple2);
+            Fruit apple3 = new Fruit("Apple",R.drawable.img_1);
+            fruitList.add(apple3);
+            Fruit apple4 = new Fruit("Apple",R.drawable.img_1);
+            fruitList.add(apple4);
+            Fruit apple5 = new Fruit("Apple",R.drawable.img_1);
+            fruitList.add(apple5);
+            Fruit apple6 = new Fruit("Apple",R.drawable.img_1);
+            fruitList.add(apple6);
+            Fruit apple7 = new Fruit("Apple",R.drawable.img_1);
+            fruitList.add(apple7);
+            Fruit apple8 = new Fruit("Apple",R.drawable.img_1);
+            fruitList.add(apple8);
+            Fruit apple9 = new Fruit("Apple",R.drawable.img_1);
+            fruitList.add(apple9);
+
+        }
     }
 
 }
