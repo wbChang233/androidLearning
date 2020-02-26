6bbf3e9a25fd72ec891769cb6dad687fd55e7d2c
wbChang <2692685036@qq.com>
2020-02-26 10:06:04 +0800

    3.3.1 布局之LinearLayout

--- app/src/main/java/com/albert/androidlearning/MainActivity.java
+++ app/src/main/java/com/albert/androidlearning/MainActivity.java
@@ -1,49 +1,16 @@
 package com.albert.androidlearning;
 
-import android.app.ProgressDialog;
-import android.content.DialogInterface;
-import android.content.Intent;
-import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ImageView;
-import android.widget.ProgressBar;
-import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener
+public class MainActivity extends AppCompatActivity
 {
 
-    private EditText editText;
-    private ImageView imageView;
-    private ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button button = findViewById(R.id.button);
-        editText = findViewById(R.id.edit_text);
-        imageView = findViewById(R.id.image_view);
-        progressBar = findViewById(R.id.progress_bar);
-        button.setOnClickListener(this);
     }
 
-    @Override
-    public void onClick(View v) {
-        switch(v.getId()){
-            case R.id.button:
-                ProgressDialog progressDialog = new ProgressDialog(MainActivity.this);
-                progressDialog.setTitle("This is ProgressDialog");
-                progressDialog.setMessage("Loading");
-                progressDialog.setCancelable(true);
-                progressDialog.show();
-                break;
-            default:
-                break;
-        }
-    }
 }
