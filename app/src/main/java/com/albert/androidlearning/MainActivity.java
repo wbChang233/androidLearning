23dd07d77dd6f36a4c85a12b5288cfdc8d6db64b
wbChang <2692685036@qq.com>
2020-02-26 15:46:01 +0800

    3.5.1 ListView的简单用法

--- app/src/main/java/com/albert/androidlearning/MainActivity.java
+++ app/src/main/java/com/albert/androidlearning/MainActivity.java
@@ -3,19 +3,23 @@ package com.albert.androidlearning;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity
 {
 
+    private String[] data = { "1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ActionBar actionBar = getSupportActionBar();
-        if(actionBar != null){
-            actionBar.hide();
-        }
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(MainActivity.this,android.R.layout.simple_list_item_1,
+                data);
+        ListView listView = findViewById(R.id.list_view);
+        listView.setAdapter(adapter);
+
     }
 
 }
