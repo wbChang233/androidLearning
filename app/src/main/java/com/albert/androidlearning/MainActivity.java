bed6d2cbb9f210080a29dc6b22439d9e43d16561
wbChang <2692685036@qq.com>
2020-02-26 09:40:38 +0800

    3.2.5 各组件使用

--- app/src/main/java/com/albert/androidlearning/MainActivity.java
+++ app/src/main/java/com/albert/androidlearning/MainActivity.java
@@ -6,21 +6,39 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
-public class MainActivity extends BaseActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener
+{
+
+    private EditText editText;
+    private ImageView imageView;
+    private ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.d("data","onCreate execute");
+        Button button = findViewById(R.id.button);
+        editText = findViewById(R.id.edit_text);
+        imageView = findViewById(R.id.image_view);
+        progressBar = findViewById(R.id.progress_bar);
+        button.setOnClickListener(this);
+    }
 
-        Button button = findViewById(R.id.button_1);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                SecondActivity.actionStart(MainActivity.this,"data1","data2");
-            }
-        });
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()){
+            case R.id.button:
+                int progress = progressBar.getProgress();
+                progress = progress +10;
+                progressBar.setProgress(progress);
+                break;
+            default:
+                break;
+        }
     }
 }
